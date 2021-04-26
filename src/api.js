2eafe105b7643fd6bfe5f112cbb501661d79efc1
baromeg <baromeg@gmail.com>
2021-04-26 17:29:42 +0100

    API route fixed

--- src/api.js
+++ src/api.js
@@ -27,9 +27,9 @@ const OMDB_KEY = process.env.REACT_APP_OMDB_KEY
 const searchOMDB = `?apikey=${OMDB_KEY}&s=batman&type=movie`
 
 // * RAWG
-const popularGames = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10?key=${RAWG_KEY}`
-const upcomingGames = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10?key=${RAWG_KEY}`
-const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10?key=${RAWG_KEY}`
+const popularGames = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10&key=${RAWG_KEY}`
+const upcomingGames = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10&key=${RAWG_KEY}`
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10&key=${RAWG_KEY}`
 
 // * Movies and TV Series
 // export const top250MoviesURL = () => `${base_url_movies}${top250Movies}`
@@ -43,11 +43,11 @@ export const newGamesURL = () => `${base_url_games}${newGames}`
 
 // * Games Details
 export const gameDetailsURL = (game_id) =>
-  `${base_url_games}games/${game_id}?key=${RAWG_KEY}`
+  `${base_url_games}games/${game_id}&key=${RAWG_KEY}`
 
 // * Game Screenshots
 export const gameScreenshotURL = (game_id) =>
-  `${base_url_games}games/${game_id}/screenshots?key=${RAWG_KEY}`
+  `${base_url_games}games/${game_id}/screenshots&key=${RAWG_KEY}`
 
 // * Search game
 export const searchGameURL = (game_name) =>
